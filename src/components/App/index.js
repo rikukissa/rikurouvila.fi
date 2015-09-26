d6cf30e0b7f8e0fa5cd164593d86b0c2f70438dd
Riku Rouvila <riku.rouvila@gmail.com>
2015-09-26 04:16:00 +0300

    add logo

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import styles from './index.styl';
+import Logo from 'components/Logo';
 
 export class App extends Component {
   render() {
     return (
       <div className={styles.app}>
+        <Logo />
       </div>
     );
   }
---
+++ src/components/Logo/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styles from './index.styl';
+
+export default class Logo extends Component {
+  render() {
+    return (
+      <div className={styles.section}>
+        <div className={styles.container}>
+          <img src={require('file!assets/images/riku.png')} />
+          <img src={require('file!assets/images/rouvila.png')} />
+        </div>
+      </div>
+    );
+  }
+}
