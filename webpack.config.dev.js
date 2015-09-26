b285b43fb98a4d1d834c02e9f1047d4c2090af52
Riku Rouvila <riku.rouvila@gmail.com>
2015-09-26 04:05:49 +0300

    initial development environment

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -16,11 +16,20 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
+  resolve: {
+    modulesDirectories: ['./src', './node_modules']
+  },
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
+    }, {
+      test: /\.styl$/,
+      loaders: [
+        'style-loader',
+        'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!stylus'
+      ]
     }]
   }
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -29,6 +29,12 @@ module.exports = {
       test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
+    }, {
+      test: /\.styl$/,
+      loaders: [
+        'style-loader',
+        'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!stylus'
+      ]
     }]
   }
 };
