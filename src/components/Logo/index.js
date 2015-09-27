8d82140b668cd56c18a7c5794c3f8c91a67c7d3b
Riku Rouvila <riku.rouvila@gmail.com>
2015-09-27 18:03:38 +0300

    cleaner layer positioning

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -4,7 +4,7 @@ import styles from './index.styl';
 export default class Logo extends Component {
   render() {
     return (
-      <div className={styles.section}>
+      <div className={styles.main}>
         <div className={styles.container}>
           <img src={require('file!assets/images/riku.png')} />
           <img src={require('file!assets/images/rouvila.png')} />
