5606e193bbe84b666c1c7b4775e5a74ac56abc78
Travis CI <rrou@FL805.local>
2016-12-15 23:09:52 +0200

    update deploy logic

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,59 @@
 #!/bin/bash
-set -e # exit with nonzero exit code if anything fails
+set -e # Exit with nonzero exit code if anything fails
 
-# run our compile script, discussed above
-npm run build
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
 
-# go to the out directory and create a *new* Git repo
+rm -rf dist
+
+function doCompile {
+  npm run build
+}
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    doCompile
+    exit 0
+fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone $REPO dist
 cd dist
-git init
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
 
-# inside this git repo we'll pretend to be a new user
+# Clean out existing contents
+rm -rf dist/**/* || exit 0
+
+# Run our compile script
+doCompile
+
+# Now let's go have some fun with the cloned repo
+cd dist
 git config user.name "Travis CI"
-git config user.email "riku.rouvila@gmail.com"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# The first and only commit to this new Git repo contains all the
-# files present with the commit message "Deploy to GitHub Pages".
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
 git add .
-git commit -m "Deploy to GitHub Pages"
+git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
